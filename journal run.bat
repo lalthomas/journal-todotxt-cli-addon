9550ae66ac31af3f832a7a7599ad8a74be060757
Lal Thomas <lal.thomas.mail@gmail.com>
2017-01-02 22:27:27 +0530

    add run file

--- journal run.bat
+++ journal run.bat
@@ -4,3 +4,11 @@ REM Creation Date : 2017-01-02
 REM Author : Lal Thomas 	
 REM Original File : D:\Dropbox\project\20160204-journal todotxt cli addon\journal 	
 	
+%~d0
+cd "%~p0"
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+pushd "%userProfile%\.todo.actions.d"
+echo "%scriptFolderPath%"
+mklink /d journal "%scriptFolderPath%"
+popd
